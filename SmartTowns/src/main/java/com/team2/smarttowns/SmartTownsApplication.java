fba4f5974df87f69e4b92b36965bc94e2bcc4615
Yingyan Liu <liuy330@cardiff.ac.uk>
2023-11-23 12:37:30 +0000

    created project

---
+++ SmartTowns/src/main/java/com/team2/smarttowns/SmartTownsApplication.java
@@ -0,0 +1,13 @@
+package com.team2.smarttowns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTownsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartTownsApplication.class, args);
+    }
+
+}
---
+++ SmartTowns/src/test/java/com/team2/smarttowns/SmartTownsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.smarttowns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTownsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
