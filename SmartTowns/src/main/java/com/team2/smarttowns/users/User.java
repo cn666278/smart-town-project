ef0411c32792ef8fef59b2cf40e33eb02a89c8b8
Chenke Sun <sunc20@cardiff.ac.uk>
2023-11-29 21:32:05 +0000

    [+]User Entity

---
+++ SmartTowns/src/main/java/com/team2/smarttowns/users/User.java
@@ -0,0 +1,18 @@
+package com.team2.smarttowns.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    private int id;
+    private String name;
+    private String password;
+    private String profile_img;
+    private String account;
+    private String email;
+    private String badget;
+
+}
