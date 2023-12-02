9801fce912056ad0ed26246030d213f257062373
Yingyan Liu <liuy330@cardiff.ac.uk>
2023-12-02 16:50:45 +0000

    created Checkpoint.java

---
+++ SmartTowns/src/main/java/com/team2/smarttowns/Checkpoint.java
@@ -0,0 +1,11 @@
+package com.team2.smarttowns;
+
+import lombok.Data;
+
+@Data
+public class Checkpoint {
+    private int checkpointId;
+    private String checkpointName;
+    private String checkpointImgUrl;
+    private String details;
+}
