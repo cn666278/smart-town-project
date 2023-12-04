a38f076c24c5de50f86b3a28036763bcefd8033a
Nuo Chen <chenn16@cardiff.ac.uk>
2023-12-05 07:58:49 +0800

    [#] edit the TrailController

--- SmartTowns/src/main/java/com/team2/smarttowns/trail/TrailController.java
+++ SmartTowns/src/main/java/com/team2/smarttowns/trail/TrailController.java
@@ -20,12 +20,14 @@ public class TrailController {
     public ModelAndView trailPage(@PathVariable int id) {
         // TODO : Rename trails.html to tail.html
         ModelAndView modelAndView = new ModelAndView("trails.html");
+        // add the list of trails to the model
+        modelAndView.addObject("trails", trail.getAllTrails());
+        // add the trail details to the model
         for (Trail trail : trailRepository.getAllTrails()) {
             if (trail.getId() == id) {
-                modelAndView.addObject("trails", trail.getAllTrails());
-//                modelAndView.addObject("name", trail.getName());s
-//                modelAndView.addObject("image", trail.getImage());
-//                modelAndView.addObject("details", trail.getDetails());
+                modelAndView.addObject("name", trail.getName());s
+                modelAndView.addObject("image", trail.getImage());
+                modelAndView.addObject("details", trail.getDetails());
             }
         }
         return modelAndView;
