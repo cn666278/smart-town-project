fa8fb4550ddfd91970912786d0a14fbb518530fa
Chenke Sun <sunc20@cardiff.ac.uk>
2023-12-06 12:44:39 +0000

    [+]add model to use in service

---
+++ SmartTowns/src/main/java/com/team2/smarttowns/model/Checkpoint.java
@@ -0,0 +1,19 @@
+package com.team2.smarttowns.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Checkpoint {
+
+    private int id;
+    private String latitude;
+    private String longitude;
+    private String image;
+    private String detail;
+    private String address;
+
+}
