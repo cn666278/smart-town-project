7d65218b59c96dc38dbed17e16c986a32f9e72cf
Shuoyan Zhao <zhaos31@cardiff.ac.uk>
2023-12-06 23:07:34 +0000

    [+]add api of getting user completed trails

--- SmartTowns/src/main/java/com/team2/smarttowns/controller/api/TrailsApi.java
+++ SmartTowns/src/main/java/com/team2/smarttowns/controller/api/TrailsApi.java
@@ -1,8 +1,12 @@
 package com.team2.smarttowns.controller.api;
 
 import com.google.gson.Gson;
+import com.team2.smarttowns.model.User;
 import com.team2.smarttowns.service.TrailService;
+import com.team2.smarttowns.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +16,11 @@ public class TrailsApi {
         public String getAllTrailsDataApi() {
                 Gson gson = new Gson();
                 return gson.toJson(trailService.getAllTrails());
+        }
+        UserService userService=new UserService();
+        @PostMapping("/api/trails/GetCompletedTrails")
+        public String getCompletedTrailsDataApi(@RequestParam("userId") int userId) {
+                Gson gson = new Gson();
+                return gson.toJson(userService.getCompletedTrailsByUserId(userId));
         }
 }
