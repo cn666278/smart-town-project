2e62f334cc5673a1fcc1b8d13b011b0bded780e2
Yingyan Liu <liuy330@cardiff.ac.uk>
2023-12-07 20:11:58 +0000

    [+] added login.html

---
+++ SmartTowns/src/main/java/com/team2/smarttowns/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.team2.smarttowns.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    String login() {
+        return "login";
+    }
+}
