4c6298f8c4801656800272e8c4bfc615f39b9c2f
Yingyan Liu <liuy330@cardiff.ac.uk>
2023-12-10 11:17:31 +0000

    [+] added SecurityConfig.java

---
+++ SmartTowns/src/main/java/com/team2/smarttowns/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.team2.smarttowns.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    public static final String[] ENDPOINTS_WHITELIST = {"/checkpoint/**", "/about", "/home",
+            "/towns", "/trails", "/trailsmap"};
+    public static final String[] USER_ENDPOINTS_WHITELIST = {"/myaccount"};
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request
+                        .requestMatchers(ENDPOINTS_WHITELIST).permitAll()
+                        .requestMatchers(USER_ENDPOINTS_WHITELIST).hasRole("USER")
+                        .anyRequest().hasRole("ADMIN"))
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.loginPage("/login").permitAll());
+        return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        JdbcDaoImpl jdbcUserDetails = new JdbcDaoImpl();
+        jdbcUserDetails.setDataSource(dataSource);
+        jdbcUserDetails.setUsersByUsernameQuery("select username, password, enabled from users where username=?");
+        jdbcUserDetails.setAuthoritiesByUsernameQuery("select username, authority from user_authorities where username=?");
+        return jdbcUserDetails;
+    }
+
+}
