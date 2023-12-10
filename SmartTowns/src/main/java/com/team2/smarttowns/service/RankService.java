21f498d4b65e69c5bf6a9baff85503b662b99cb5
Jiarui Hao <HaoJ6@cardiff.ac.uk>
2023-12-10 21:46:55 +0800

    [#]typo

--- SmartTowns/src/main/java/com/team2/smarttowns/service/RankService.java
+++ SmartTowns/src/main/java/com/team2/smarttowns/service/RankService.java
@@ -1,8 +1,10 @@
 package com.team2.smarttowns.service;
 
+import com.team2.smarttowns.dao.CheckpointRepository;
 import com.team2.smarttowns.dao.CheckpointRepositoryImpl;
 import com.team2.smarttowns.entity.CheckpointEntity;
 import com.team2.smarttowns.model.Checkpoint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,14 +15,30 @@ import java.util.List;
  */
 @Service
 public class RankService {
+    private final CheckpointRepository checkpointRepository;
+
+    @Autowired
+    public RankService(CheckpointRepository checkpointRepository) {
+        this.checkpointRepository = checkpointRepository;
+    }
+
+    public List<Checkpoint> getAllUserCheckpoints() {
+        List<Checkpoint> checkpoints = new ArrayList<>();
+
+        // 需要所有用户访问过的检查点
+        List<CheckpointEntity> checkpointEntities = checkpointRepository.getAllCheckpoints();
+
+        for (CheckpointEntity checkpointEntity : checkpointEntities) {
+            checkpoints.add(checkpointEntityToModel(checkpointEntity));
+        }
+
+        return checkpoints;
+    }
 
     public List<Checkpoint> checkpointUserAccessed(int userID) {
         List<Checkpoint> checkpoints = new ArrayList<>();
 
         // 需要用户访问过的检查点
-
-        CheckpointRepositoryImpl checkpointRepository = new CheckpointRepositoryImpl();
-
         List<CheckpointEntity> checkpointEntities = checkpointRepository.getUserAccessedCheckpointsByUserId(userID);
 
         for (CheckpointEntity checkpointEntity : checkpointEntities) {
@@ -29,6 +47,7 @@ public class RankService {
 
         return checkpoints;
     }
+
 
 
 
