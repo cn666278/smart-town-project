ed1760ea9cf913f57a7ebe7b48ec5b450b82eba0
Nuo Chen <chenn16@cardiff.ac.uk>
2023-12-12 07:50:55 +0800

    [#] edit the towns controller

--- SmartTowns/src/main/java/com/team2/smarttowns/controller/web/TownsController.java
+++ SmartTowns/src/main/java/com/team2/smarttowns/controller/web/TownsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class TownsController {
 
     @GetMapping("/towns")
-    public ModelAndView homePage() {
+    public ModelAndView townsPage() {
         ModelAndView modelAndView = new ModelAndView("town.html");
         return modelAndView;
     }
