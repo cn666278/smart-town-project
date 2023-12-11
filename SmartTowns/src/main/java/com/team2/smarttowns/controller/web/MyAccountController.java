5903ee0791504c70da742a2c71432b2477784879
Shuoyan Zhao <zhaos31@cardiff.ac.uk>
2023-12-11 20:26:41 +0000

    [+]Gets the completed trails based on the user ID and passes the list of trail models to the view layer to present the completed trails information in the "myaccount.html" view.

--- SmartTowns/src/main/java/com/team2/smarttowns/controller/web/MyAccountController.java
+++ SmartTowns/src/main/java/com/team2/smarttowns/controller/web/MyAccountController.java
@@ -1,16 +1,31 @@
 package com.team2.smarttowns.controller.web;
 
+import com.team2.smarttowns.dao.CheckpointRepository;
+import com.team2.smarttowns.dao.TrailRepository;
+import com.team2.smarttowns.entity.TrailEntity;
+import com.team2.smarttowns.model.Trail;
+import com.team2.smarttowns.service.TrailService;
 import com.team2.smarttowns.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class MyAccountController {
 
     UserService userService;
+
+
+    CheckpointRepository checkpointRepository;
+    TrailRepository trailRepository;
+
+    TrailService trailService;
 
     @RequestMapping("/myaccount")
     public ModelAndView myaccount() {
@@ -26,3 +41,22 @@ public class MyAccountController {
     }
 
 }
+
+
+    @GetMapping("/myaccount/completedTrails/{userid}")
+    public ModelAndView completedTrailsPage(@PathVariable int userid) {
+        ModelAndView modelAndView = new ModelAndView("myaccount.html");
+
+        List<Integer> userCompletedTrails = trailRepository.getCompletedTrailsByUserId(userid);
+//
+        List<Trail> completedTrails = new ArrayList<>();
+
+        for (Integer trailId : userCompletedTrails) {
+            Trail trail = trailService.getTrailById(trailId);
+            completedTrails.add(trail);
+        }
+        modelAndView.addObject("completedTrails", completedTrails);
+        return modelAndView;
+    }
+
+
