27d6fb3c8dea973c611bf8ccc965d3320ee17a3a
Jiarui Hao <HaoJ6@cardiff.ac.uk>
2023-12-13 01:24:50 +0800

    [#]typo

--- SmartTowns/src/main/resources/schema.sql
+++ SmartTowns/src/main/resources/schema.sql
@@ -95,7 +95,7 @@ CREATE TABLE `trail`  (
                           `id` int(11) NOT NULL AUTO_INCREMENT,
                           `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
                           `image` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-                          `details` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+                          `detail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
                           PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
