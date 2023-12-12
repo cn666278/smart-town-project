08c80552b62fc8bb16efd4b495e95c8f25e0d9ec
Nuo Chen <chenn16@cardiff.ac.uk>
2023-12-12 08:35:33 +0800

    [+] add TownsController

--- SmartTowns/src/main/java/com/team2/smarttowns/controller/web/TownsController.java
+++ SmartTowns/src/main/java/com/team2/smarttowns/controller/web/TownsController.java
@@ -1,5 +1,6 @@
 package com.team2.smarttowns.controller.web;
 
+import com.team2.smarttowns.service.TownService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class TownsController {
 
+    TownService townService;
+
     @GetMapping("/towns")
     public ModelAndView townsPage() {
-        ModelAndView modelAndView = new ModelAndView("town.html");
+        ModelAndView modelAndView = new ModelAndView("towns.html");
+        modelAndView.addObject("towns", townService.getAllTowns());
         return modelAndView;
     }
 
