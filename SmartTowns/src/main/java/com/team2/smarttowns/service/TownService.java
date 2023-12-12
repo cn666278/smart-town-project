1cede7e5fdc1d34c50133555cbbf617cadbd7082
Nuo Chen <chenn16@cardiff.ac.uk>
2023-12-12 09:38:37 +0800

    [#] rewrite the TownService

--- SmartTowns/src/main/java/com/team2/smarttowns/service/TownService.java
+++ SmartTowns/src/main/java/com/team2/smarttowns/service/TownService.java
@@ -2,19 +2,46 @@ package com.team2.smarttowns.service;
 
 import com.team2.smarttowns.dao.TownRepository;
 import com.team2.smarttowns.entity.TownEntity;
+import com.team2.smarttowns.model.Town;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class TownService {
 
     TownRepository townRepository;
 
-    public List<TownEntity> getAllTowns() {
-        return townRepository.getAllTowns();
+    public List<Town> getAllTowns() {
+        List<Town> towns = new ArrayList<>();
+        //get data from dao. Then transfer entity to model
+        List<TownEntity> townEntities = townRepository.getAllTowns();
+
+        for (TownEntity townEntity : townEntities) {
+            Town town = new Town();
+            townEntityToModel(townEntity, town);
+            towns.add(town);
+        }
+
+        return towns;
+
+    }
+
+    private void townEntityToModel(TownEntity townEntity, Town town) {
+        town.setId(townEntity.getId());
+        town.setName(townEntity.getName());
+        town.setImage(townEntity.getImage());
+        town.setDetail(townEntity.getDetail());
     }
 
     public TownEntity getTownById(int id) {
         return townRepository.getTownById(id);
     }
 
+    @Autowired
+    public void setTownRepository(TownRepository townRepository) {
+        this.townRepository = townRepository;
+    }
 }
