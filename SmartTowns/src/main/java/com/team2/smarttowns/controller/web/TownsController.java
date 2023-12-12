b2b6fef3a66ea82d6ff0b034168d2c9f46e0d90d
Nuo Chen <chenn16@cardiff.ac.uk>
2023-12-12 09:52:28 +0800

    [#] fixed the bugs in townRepository.getAllTowns() is null

--- SmartTowns/src/main/java/com/team2/smarttowns/controller/web/TownsController.java
+++ SmartTowns/src/main/java/com/team2/smarttowns/controller/web/TownsController.java
@@ -1,6 +1,9 @@
 package com.team2.smarttowns.controller.web;
 
+import com.team2.smarttowns.dao.TownRepository;
+import com.team2.smarttowns.entity.TownEntity;
 import com.team2.smarttowns.service.TownService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +12,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class TownsController {
 
+    @Autowired
+    private TownRepository townRepository;
     TownService townService;
 
     @GetMapping("/towns")
     public ModelAndView townsPage() {
         ModelAndView modelAndView = new ModelAndView("towns.html");
-        modelAndView.addObject("towns", townService.getAllTowns());
+        modelAndView.addObject("towns", townRepository.getAllTowns());
         return modelAndView;
     }
 
