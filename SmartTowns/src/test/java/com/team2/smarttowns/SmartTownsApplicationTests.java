901ed977e9fe23f8e32064de736dedb3f5d8477d
Shuoyan Zhao <zhaos31@cardiff.ac.uk>
2023-12-12 20:18:50 +0000

    [+]Import injection

--- SmartTowns/src/test/java/com/team2/smarttowns/SmartTownsApplicationTests.java
+++ SmartTowns/src/test/java/com/team2/smarttowns/SmartTownsApplicationTests.java
@@ -3,6 +3,8 @@ package com.team2.smarttowns;
 import com.team2.smarttowns.dao.CheckpointRepository;
 import com.team2.smarttowns.dao.CheckpointRepositoryImpl;
 import com.team2.smarttowns.entity.CheckpointEntity;
+import com.team2.smarttowns.model.User;
+import com.team2.smarttowns.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,12 +14,20 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
 class SmartTownsApplicationTests {
     @Autowired
     private CheckpointRepository checkpointRepository;
+
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private UserService userservice;
 
     /**
      * Test to retrieve all checkpoints
@@ -35,32 +45,32 @@ class SmartTownsApplicationTests {
      */
     @Test
     void addCheckpoint() {
-        checkpointRepository.addCheckpoint(new CheckpointEntity(0,
-                "Cardiff Bay",
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Cardiff_Bay_121020_2469_%2850599762103%29_-_Flickr_-_Richard_Szwejkowski.jpg/1920px-Cardiff_Bay_121020_2469_%2850599762103%29_-_Flickr_-_Richard_Szwejkowski.jpg",
-                "Cardiff Bay (Welsh: Bae Caerdydd; colloquially \"The Bay\") is an area and freshwater lake[1][2] in Cardiff, Wales. The site of a former tidal bay and estuary, it is the river mouth of the River Taff and Ely.",
-                "https://www.cardiffbay.co.uk/",
-                "51.4539° N",
-                "3.1694° W",
-                "Cardiff CF10 3EU"));
+//        checkpointRepository.addCheckpoint(new CheckpointEntity(0,
+//                "Cardiff Bay",
+//                "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Cardiff_Bay_121020_2469_%2850599762103%29_-_Flickr_-_Richard_Szwejkowski.jpg/1920px-Cardiff_Bay_121020_2469_%2850599762103%29_-_Flickr_-_Richard_Szwejkowski.jpg",
+//                "Cardiff Bay (Welsh: Bae Caerdydd; colloquially \"The Bay\") is an area and freshwater lake[1][2] in Cardiff, Wales. The site of a former tidal bay and estuary, it is the river mouth of the River Taff and Ely.",
+//                "https://www.cardiffbay.co.uk/",
+//                "51.4539° N",
+//                "3.1694° W",
+//                "Cardiff CF10 3EU"));
         getAllCheckpoints();
     }
 
     /**
      * Test to update an existing checkpoint
      */
-    @Test
-    void updateCheckpoint() {
-        checkpointRepository.updateCheckpoint(new CheckpointEntity(1,
-                "Cardiff Bay",
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Cardiff_Bay_121020_2469_%2850599762103%29_-_Flickr_-_Richard_Szwejkowski.jpg/1920px-Cardiff_Bay_121020_2469_%2850599762103%29_-_Flickr_-_Richard_Szwejkowski.jpg",
-                "Cardiff Bay (Welsh: Bae Caerdydd; colloquially \"The Bay\") is an area and freshwater lake[1][2] in Cardiff, Wales. The site of a former tidal bay and estuary, it is the river mouth of the River Taff and Ely.",
-                "https://www.cardiffbay.co.uk/",
-                "51.4539° N",
-                "3.1694° W",
-                "Cardiff CF10 3EU"));
-        getAllCheckpoints();
-    }
+//    @Test
+//    void updateCheckpoint() {
+//        checkpointRepository.updateCheckpoint(new CheckpointEntity(1,
+//                "Cardiff Bay",
+//                "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Cardiff_Bay_121020_2469_%2850599762103%29_-_Flickr_-_Richard_Szwejkowski.jpg/1920px-Cardiff_Bay_121020_2469_%2850599762103%29_-_Flickr_-_Richard_Szwejkowski.jpg",
+//                "Cardiff Bay (Welsh: Bae Caerdydd; colloquially \"The Bay\") is an area and freshwater lake[1][2] in Cardiff, Wales. The site of a former tidal bay and estuary, it is the river mouth of the River Taff and Ely.",
+//                "https://www.cardiffbay.co.uk/",
+//                "51.4539° N",
+//                "3.1694° W",
+//                "Cardiff CF10 3EU"));
+//        getAllCheckpoints();
+//    }
 
     /**
      * Test to delete a checkpoint
@@ -91,6 +101,22 @@ class SmartTownsApplicationTests {
         String rawPassword = "user1";
         String encodedPassword = passwordEncoder.encode(rawPassword);
         System.out.println(encodedPassword);
+    }
+
+
+    @Test
+    void testUpdateUser() {
+        //'user1',
+        //        '$2a$10$o5LRNO84QaTX62LzsF2AbuJL8qiU/CLpVMB7l4LKIAP8hxx0IfITu',
+        //        'https://cn.i.cdn.ti-platform.com/cnapac/content/701/showpage/we-bare-bears/sa/showicon.png',
+        //        123,
+        //        'user@cardiff.ac.uk',
+        //        'dragon hero'
+
+        User user = userservice.getUserById(1);
+        user.setName("user1");
+        user.setEmail("1111111@qq.com");
+        userservice.updateUser(user);
     }
 
 }
