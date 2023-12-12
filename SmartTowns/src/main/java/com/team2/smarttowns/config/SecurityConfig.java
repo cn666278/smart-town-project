fd1db50e1067c34bd1af32382114f558588b4cc5
Shuoyan Zhao <zhaos31@cardiff.ac.uk>
2023-12-12 18:19:19 +0000

    [+]change the spring SecurityConfig

--- SmartTowns/src/main/java/com/team2/smarttowns/config/SecurityConfig.java
+++ SmartTowns/src/main/java/com/team2/smarttowns/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
     private DataSource dataSource;
 
     public static final String[] ENDPOINTS_WHITELIST = {"/checkpoint/**", "/about", "/home",
-            "/towns", "/trails", "/trailsmap","/static/**","/css/**","/img/**","/js/**","/trails/**","/error","/rankweb","/trailsmap/**","/js/**"};
+            "/towns", "/trails", "/trailsmap","/static/**","/css/**","/img/**","/js/**","/trails/**","/error","/rankweb","/trailsmap/**","/js/**","/myaccount","/myaccount/user/**"};
     public static final String[] USER_ENDPOINTS_WHITELIST = {"/myaccount"};
 
 
