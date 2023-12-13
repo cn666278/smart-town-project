9c80e3a5810b42283cd4d24a09c1b763d164ff4a
Nuo Chen <chenn16@cardiff.ac.uk>
2023-12-14 06:25:45 +0800

    [#] link the HomeController to get access data of trails

--- SmartTowns/src/main/java/com/team2/smarttowns/controller/web/HomeController.java
+++ SmartTowns/src/main/java/com/team2/smarttowns/controller/web/HomeController.java
@@ -1,5 +1,7 @@
 package com.team2.smarttowns.controller.web;
 
+import com.team2.smarttowns.service.TrailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +10,18 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
+    TrailService trailService;
+
     @GetMapping("/home")
     public ModelAndView homePage() {
         ModelAndView modelAndView = new ModelAndView("home.html");
-        // modelAndView.addObject("message", "Hello World");
+         modelAndView.addObject("trails", trailService.getAllTrails());
         return modelAndView;
+    }
+
+    @Autowired
+    public HomeController(TrailService trailService) {
+        this.trailService = trailService;
     }
 
 }
