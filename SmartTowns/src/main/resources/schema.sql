38e3c50e59a0d914d7b2d123760d7b815fdc90cd
Shuoyan Zhao <zhaos31@cardiff.ac.uk>
2023-12-13 18:12:10 +0000

    [#]rename some data

--- SmartTowns/src/main/resources/schema.sql
+++ SmartTowns/src/main/resources/schema.sql
@@ -169,7 +169,7 @@ DROP TABLE IF EXISTS `collection_user`;
 CREATE TABLE `collection_user`  (
                                     `id` int(11) NOT NULL AUTO_INCREMENT,
                                     `user_id` int(11) NULL DEFAULT NULL,
-                                    `checkpoint_id` int(11) NULL DEFAULT NULL,
+                                    `trail_id` int(11) NULL DEFAULT NULL,
                                     PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
