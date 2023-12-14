90bd20ba4b367be34f8807d0819014ac6cf7d180
Nuo Chen <chenn16@cardiff.ac.uk>
2023-12-14 09:09:05 +0800

    [#] edit the getTrailsByUserId()

--- SmartTowns/src/main/java/com/team2/smarttowns/service/UserService.java
+++ SmartTowns/src/main/java/com/team2/smarttowns/service/UserService.java
@@ -5,8 +5,10 @@ import com.team2.smarttowns.dao.TrailRepository;
 import com.team2.smarttowns.dao.TrailRepositoryImpl;
 import com.team2.smarttowns.dao.UserRepository;
 import com.team2.smarttowns.entity.CheckpointEntity;
+import com.team2.smarttowns.entity.TrailEntity;
 import com.team2.smarttowns.entity.UserEntity;
 import com.team2.smarttowns.model.Checkpoint;
+import com.team2.smarttowns.model.Trail;
 import com.team2.smarttowns.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,9 +36,63 @@ public class UserService {
      *
      * @param id user id
      */
-    public List<Integer> getTrailsByUserId(int id) {
-        return trailRepository.getTrailsByUserId(id);
+    public Trail getTrailsByUserId(int id) {
+        Trail trail = new Trail();
+        List<TrailEntity> trailEntity = trailRepository.getTrailsByUserId(id);
+for(TrailEntity trailEntity1:trailEntity){
+    trailEntityToModel(trailEntity1,trail);
+}
+        return trail;
     }
+
+    /**
+     * Transfer trail entity to trail model
+     *
+     * @param trailEntity
+     * @param trail
+     */
+    private void trailEntityToModel(TrailEntity trailEntity, Trail trail) {
+        trail.setId(trailEntity.getId());
+        trail.setName(trailEntity.getName());
+        trail.setImage(trailEntity.getImage());
+        trail.setDetails(trailEntity.getDetails());
+        for (CheckpointEntity checkpointEntity : trailRepository.getCheckpointsByTrailId(trailEntity.getId())) {
+            Checkpoint checkpoint = checkpointEntityToModel(checkpointEntity);
+            trail.getCheckpoints().add(checkpoint);
+        }
+    }
+
+    /**
+     * Transfer checkpoint entity to checkpoint model
+     *
+     * @param checkpointEntity
+     * @return checkpoint
+     */
+    private Checkpoint checkpointEntityToModel(CheckpointEntity checkpointEntity) {
+        Checkpoint checkpoint = new Checkpoint();
+        checkpoint.setId(checkpointEntity.getId());
+        checkpoint.setName(checkpointEntity.getName());
+        checkpoint.setImage(checkpointEntity.getImage());
+        checkpoint.setDescription(checkpointEntity.getDescription());
+        return checkpoint;
+    }
+
+    /**
+     * get user collected trails by user id
+     *
+     * @param userId
+     * @return
+     */
+    public Trail getCollectedTrailsByUserId(int userId) {
+        Trail trail = new Trail();
+        List<TrailEntity> trailEntities = trailRepository.getCollectedTrailsByUserId(userId);
+        for (TrailEntity trailEntity : trailEntities) {
+            trailEntityToModel(trailEntity, trail);
+
+}
+        return trail;
+    }
+
 
     public List<Integer> getCompletedTrailsByUserId(int id) {
 
